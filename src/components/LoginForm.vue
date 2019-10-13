3037ed9fd7cfaca5e74b8145cc4b2c83c9a3985c
TimL <tim93larsson@gmail.com>
2019-10-13 15:37:12 +0200

    Dashboard

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,15 +1,13 @@
 <template>
     <div class="login-form">
         <div class="text-center">
-            <img class="mb-4" src="fly.png" alt="" width="72" height="72">
+            <img src="fly.png" alt="" width="112" height="112">
         </div>
-        <h5 class="text-center">Chat Login</h5>
         <hr>
         <b-form @submit.prevent="onsubmit">
+            <div class="text-center">
             <b-alert variant="danger" :show="hasError"> {{ error }} </b-alert>
             <b-form-group id="userInputGroup"
-                          label="User Name"
-                          label-for="userInput">
                 <b-form-input id="userInput"
                               type="text"
                               placeholder="User Name"
@@ -20,13 +18,14 @@
                 </b-form-input>
             </b-form-group>
 
-            <b-button type="submit"
-                      variant="primary"
-                      class="ld-ext-right"
-                      v-bind:class=" { running: loading}"
-                      :disabled="isValid">
-                      Login <div class="ld ld.ring ld-spin"></div>
-            </b-button>
+                <b-button type="submit"
+                          variant="primary"
+                          class="ld-ext-right"
+                          v-bind:class=" { running: loading}"
+                          :disabled="isValid">
+                    Login <div class="ld ld.ring ld-spin"></div>
+                </b-button>
+            </div>
         </b-form>
     </div>
 </template>
---
+++ src/components/UserList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="user-list">
+        <h4>Members</h4>
+        <hr>
+        <b-list-group v-for="user in users" :key="user.username">
+            {{ user.name }}
+            <b-badge v-if="user.presence"
+                     :variant="statusColor(user.presence)"
+                     pill>
+                {{ user.presence }}
+            </b-badge>
+        </b-list-group>
+    </div>
+</template>
+
+<script>
+    import { mapState } from 'vuex'
+
+    export default {
+        name: 'user-list',
+        compudet: {
+            ...mapState([
+                'loading',
+                'users'
+            ])
+        },
+        methods: {
+            statusColor(status) {
+                return status === 'online' ? 'success' : 'warning'
+            }
+        }
+    }
+</script>
+
+<style>
+    .message-list {
+        margin-bottom: 15px;
+        padding-right: 15px;
+    }
+
+    .message-group {
+        height: 65vh !important;
+        overflow-y: scroll;
+    }
+</style>
