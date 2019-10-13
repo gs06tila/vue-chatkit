0bbb348871113b1063ea46d292f9f5754f7de972
TimL <tim93larsson@gmail.com>
2019-10-13 02:33:12 +0200

    login page

---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Vuex from 'vuex' //Vuex is a state management pattern
+import VuexPersistence from "vuex-persist"; //vuex-persist package ensures that our Vuex state is saved between page reloads or refreshes
+import mutations from './mutations'
+import actions from './actions'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage
+})
+
+export default new Vuex.Store({
+    // state
+    state: {
+        loading: false, //used by the UI to determine wheter it should run the CSS loader.
+        sending: false,
+        error: null, //Store information of an error that has just occured.
+        user: [],
+        reconnect: false,
+        activeRoom: null,
+        rooms: [],
+        users:[],
+        messages: [],
+        userTyping: null
+    },
+    //used to commit and track state changes.
+    mutations,
+    // actions can update the vuex state
+    actions,
+    //getters can access our state
+    getters: {
+        hasError: state => state.error ? true : false
+    },
+    plugins: [vuexLocal.plugin],
+    strict: debug
+})
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login">
+        <b-container align-v="center" align-h="center">
+            <b-row>
+                <b-col lg="4" md="3"></b-col>
+                <b-col lg="4" md="6">
+                    <LoginForm />
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import LoginForm from '@/components/LoginForm.vue'
+
+    export default {
+        name: 'login',
+        components: {
+            LoginForm
+        }
+    }
+</script>
